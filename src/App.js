7cf23185c55aa80ceea7c8ed414f6cfe5cf49a54
Ibrahem <bremoo87@hotmail.com>
2020-01-29 11:32:32 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
+const Person = ({ img, name, job, children }) => {
+  const url = `https://randomuser.me/api/portraits/thumb/men/${img}.jpg`;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="person">
+      <img src={url} alt="person" />
+      <h4>{name}</h4>
+      <h4>{job}</h4>
+      {children}
     </div>
   );
+};
+
+const PersonList = () => {
+  return (
+    <div className="person-list">
+      <Person img="34" name="john" job="developer" />
+      <Person img="43" name="ggggg" job="developer">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam,
+          voluptatibus!
+        </p>
+      </Person>
+      <Person img="66" name="bbbbb" job="Harid Dresser" />
+    </div>
+  );
+};
+
+class App extends Component {
+  render() {
+    return <PersonList />;
+  }
 }
 
 export default App;
